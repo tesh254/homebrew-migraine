ba97547196f689eb4a8e469ae02b8b359322e863
tesh254 <ewachira254@gmail.com>
2023-09-28 00:29:22 +0300

    upgrade migraine alpha version v0.0.2-alpha.5

--- Formula/migraine.rb
+++ Formula/migraine.rb
@@ -1,15 +1,15 @@
 class Migraine < Formula
     desc "migraine is a command-line interface (CLI) tool designed to help you manage migrations in your backend project using a PostgreSQL database."
     homepage "https://github.com/tesh254/migraine"
-    url "https://github.com/tesh254/migraine/releases/download/v0.0.2-alpha.4/migraine_0.0.2-alpha.4_darwin_amd64.tar.gz"
-    sha256 "0ec8e25323a6499c2d85d1f551c3f53d19e0da722eec5f45f8a8d3049e0ae901"
-    version "v0.0.2-alpha.4"
+    url "https://github.com/tesh254/migraine/releases/download/v0.0.2-alpha.5/migraine_0.0.2-alpha.5_darwin_amd64.tar.gz"
+    sha256 "a6544b14388e58845358a51d80705fb5f6c8c00185288086376be1a4d4d698c3"
+    version "v0.0.2-alpha.5"
     version_scheme 1
   
     on_macos do
       if Hardware::CPU.arm?
-        url "https://github.com/tesh254/migraine/releases/download/v0.0.2-alpha.4/migraine_0.0.2-alpha.4_darwin_arm64.tar.gz"
-        sha256 "1dde3a8f6fd68842d7f54c5a60c50eb09889e348524a0c07aa4aa433f92cc8bf"
+        url "https://github.com/tesh254/migraine/releases/download/v0.0.2-alpha.5/migraine_0.0.2-alpha.5_darwin_arm64.tar.gz"
+        sha256 "63e74cad123c160361b0b2b87eb3254b6d47dd1f8534fd9c3c23f17a0e45e149"
       end
     end
   
