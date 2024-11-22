449d24b6fc2a9223bca9e19fe744cd6542ea7e7a
GitHub Action <action@github.com>
2024-11-22 01:07:07 +0000

    Update migraine to v0.0.12

--- migraine.rb
+++ migraine.rb
@@ -1,26 +1,26 @@
 class Migraine < Formula
   desc "A robust CLI tool used to organize and automate complex workflows with templated commands"
   homepage "https://github.com/tesh254/migraine"
-  version "0.0.11"
+  version "0.0.12"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/tesh254/migraine/releases/download/v0.0.11/migraine-darwin-amd64"
-      sha256 "6b337d4d06372b4051623ffc8c9f010c0396738e6855940be959587652b5b0fa"
+      url "https://github.com/tesh254/migraine/releases/download/v0.0.12/migraine-darwin-amd64"
+      sha256 "4c3082d7f6c0a1b05fb366cf925834a1051e785b4534b31b44a31fc1c66cc4cd"
     else
-      url "https://github.com/tesh254/migraine/releases/download/v0.0.11/migraine-darwin-arm64"
-      sha256 "8f88b2d71268c2b2c417efb74f62c3d6877c63cb2b3c196921e6470b978bc92f"
+      url "https://github.com/tesh254/migraine/releases/download/v0.0.12/migraine-darwin-arm64"
+      sha256 "6e008a4dccdd004d4ea25a76fc6db2a5896cccf3d84e38e1220f7ffe472e09bc"
     end
   end
 
   def install
     bin.install Dir["migraine-*"][0] => "migraine"
-    # Create the mig alias
-    bin.install_symlink "migraine" => "mig"
+    # Create the mgr alias
+    bin.install_symlink "migraine" => "mgr"
   end
 
   test do
     system "#{bin}/migraine", "--version"
-    system "#{bin}/mig", "--version"
+    system "#{bin}/mgr", "--version"
   end
 end
