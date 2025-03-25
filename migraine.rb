cb7fca9f81a7740d1696e30243147bfc19a6aade
GitHub Action <action@github.com>
2025-03-25 18:42:22 +0000

    Update migraine to v0.1.2

--- migraine.rb
+++ migraine.rb
@@ -1,15 +1,15 @@
 class Migraine < Formula
   desc "A robust CLI tool used to organize and automate complex workflows with templated commands"
   homepage "https://github.com/tesh254/migraine"
-  version "0.1.1"
+  version "0.1.2"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/tesh254/migraine/releases/download/v0.1.1/migraine-darwin-amd64"
-      sha256 "73be442ccbb13bc0c21a24d34d839608d212c7e58265b2d417c66c66cdabee96"
+      url "https://github.com/tesh254/migraine/releases/download/v0.1.2/migraine-darwin-amd64"
+      sha256 "a186cd0d6d6346e7c72f82b69ada56e877d5075957edd5bd51283d4f4ff7ad3b"
     else
-      url "https://github.com/tesh254/migraine/releases/download/v0.1.1/migraine-darwin-arm64"
-      sha256 "a77aa33b25af7cbbc568e5cfef860e0ce056c3ca6f52d38ae70d00056f20f9c1"
+      url "https://github.com/tesh254/migraine/releases/download/v0.1.2/migraine-darwin-arm64"
+      sha256 "d84d2c4428e047fd530e218727d292db1805f2a6a11e5f0faa01143d5ad26c0d"
     end
   end
 
