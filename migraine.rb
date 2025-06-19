28e602c7e617af30b36b7d1c5ac9d3601224e466
GitHub Action <action@github.com>
2025-06-19 05:50:10 +0000

    Update migraine to v1.0.6 (BuildInfo support)

--- migraine.rb
+++ migraine.rb
@@ -1,15 +1,15 @@
 class Migraine < Formula
   desc "A robust CLI tool used to organize and automate complex workflows with templated commands"
   homepage "https://github.com/tesh254/migraine"
-  version "1.0.5"
+  version "1.0.6"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/tesh254/migraine/releases/download/v1.0.5/migraine-darwin-amd64"
-      sha256 "bc7ad7b3a2c2b652d76bb13f1875dd438c8f14715467cbcbfc0da3b9a5e15d95"
+      url "https://github.com/tesh254/migraine/releases/download/v1.0.6/migraine-darwin-amd64"
+      sha256 "b2a862e4d232bb48c68714aa40a39b50ab9e15fc53f6390b4c6b02f1159b8e72"
     else
-      url "https://github.com/tesh254/migraine/releases/download/v1.0.5/migraine-darwin-arm64"
-      sha256 "7eab8dedb8bcbaf5aaabbc0819692f9e0b23b245b4e15d956f2b313ee1a484b0"
+      url "https://github.com/tesh254/migraine/releases/download/v1.0.6/migraine-darwin-arm64"
+      sha256 "eeaf1f0911cef029ffdbac221e321f24fc48e66025ba747a4e541f9f1bf49129"
     end
   end
 
@@ -20,7 +20,17 @@ class Migraine < Formula
   end
 
   test do
+    # Test basic version command
     system "#{bin}/migraine", "--version"
     system "#{bin}/mgr", "--version"
+    
+    # Test BuildInfo version commands
+    assert_match version.to_s, shell_output("#{bin}/migraine version --short")
+    assert_match "Platform:", shell_output("#{bin}/migraine version")
+    
+    # Test JSON output is valid
+    json_output = shell_output("#{bin}/migraine version --json")
+    assert_match "version", json_output
+    assert_match "platform", json_output
   end
 end
